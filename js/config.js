592f3bf513bbad19bbc799cb35961af916293183
Victor Goizueta <81833828+vgoizueta@users.noreply.github.com>
2024-03-27 22:23:13 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_MX1zjZbgi", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "3jq7qohgj4kf8359ggj1pcvl46", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    userPoolId: "us-east-1_TXDJQgptZ", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "v3f8f0slk7m7dnbt96naa7a9c", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "us-east-1", // e.g. us-east-2
   },
   api: {
